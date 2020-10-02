a3478ba8ee4737eeec5b21e79bb660c619870cf7
Nitin Sai <musicssns@gmail.com>
2020-10-02 19:33:22 +0530

    linear regression

--- Linear Regression/python/Linear_Regression.py
+++ Linear Regression/python/Linear_Regression.py
@@ -1,96 +1,71 @@
+# Importing the dependancies
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 
-# Load
-X = pd.read_csv('./Training Data/Linear_X_Train.csv')
-y = pd.read_csv('./Training Data/Linear_Y_Train.csv')
+# Loading the data in numerical format
+X = pd.read_csv('Linear Regression\python\Training Data\Linear_X_Train.csv').values
+y = pd.read_csv('Linear Regression\python\Training Data\Linear_Y_Train.csv').values
 
-# Convert X,Y to Numpy arrays
-X = X.values
-y = y.values
-
-# Normalisation
+# Standardising the data (We can even use min-max normalization)
 u = X.mean()
 std = X.std()
 X = (X-u)/std
 
-
-# Visualise
-plt.style.use('seaborn')
-plt.scatter(X,y,color='orange')
+# Visualising the data
+plt.style.use('fivethirtyeight') # Setting a plot style
+plt.scatter(X, y)
 plt.title("Hardwork vs Performance Graph")
 plt.xlabel("Hardwork")
 plt.ylabel("Performance")
 plt.show()
 
-def hypothesis(x,theta):
-    y_ = theta[0] + theta[1]*x
-    return y_
+# METHOD-1: Normal Equation
 
-def gradient(X,Y,theta):
-    m = X.shape[0]
-    grad = np.zeros((2,))
-    for i in range(m):
-        x = X[i]
-        y_ = hypothesis(x,theta)
-        y = Y[i]
-        grad[0] += (y_ - y)
-        grad[1] += (y_ - y)*x
-    return grad/m
+X_norm = np.concatenate((np.ones((X.shape[0], 1)), X), axis=1)
+theta_norm = np.dot(np.linalg.inv(np.dot(X_norm.transpose(), X_norm)), np.dot(X_norm.transpose(), y))
+print(f'Normal Equation method: {theta_norm}') # Parameter values
 
-def error(X,Y,theta):
-    m = X.shape[0]
-    total_error = 0.0
-    for i in range(m):
-        y_ = hypothesis(X[i],theta)
-        total_error += (y_ - Y[i])**2
-        
-    return (total_error/m)
+# METHOD-2: Gradient Descent
+
+# Hypothesis function
+def cost_function(temp, learning_rate):
+    cost_sigma = 0
+    for i in range(X_norm.shape[0]):
+        cost_sigma += (np.dot(temp.transpose(), X_norm[i])-y[i])**2
+    cost = learning_rate*(1/(2*X_norm.shape[0]))*cost_sigma
+    return cost
+
+# Updating the parameters 
+def update_theta(theta, learning_rate):
+    temp = np.empty((theta.shape[0], 1))
+    for i in range(theta.shape[0]):
+        sigma = np.zeros(1, dtype=np.float64)
+        for j in range(X_norm.shape[0]):
+            sigma += (np.dot(theta.transpose(), X_norm[j])-y[j])*X_norm[j][i]
+        temp[i] = (theta[i] - (learning_rate/X_norm.shape[0])*sum(sigma))
+    return temp
     
+# Iterating 1000 times (Can be reduced if the rate of change of cost is very low)
+cost = [] # appending the costs for visualization
+theta = np.zeros((X_norm.shape[1], 1)) # initializing the parameters
+n = int(input("Enter the number of iterations: "))
+for k in range(n):
+    learning_rate = 0.1 # (Ideal but can be triggered)
+    theta = update_theta(theta, learning_rate)
+    cost.append(cost_function(theta, learning_rate))
 
-def gradientDescent(X,Y,max_steps=100,learning_rate =0.1):
-    
-    theta = np.zeros((2,))
-    error_list = []
-    theta_list = []
-    
-   
-    for i in range(max_steps):
-        
-        # Compute grad
-        grad = gradient(X,Y,theta)
-        e = error(X,Y,theta)[0]
-        
-        
-        #Update theta
-        theta[0] = theta[0] - learning_rate*grad[0]
-        theta[1] = theta[1] - learning_rate*grad[1]
-        # Storing the theta values during updates
-        theta_list.append((theta[0],theta[1]))
-        error_list.append(e)
-        
-    return theta,error_list,theta_list
+print(f'Gradient Descent method: {theta}') # Parameter values
 
-
-theta,error_list,theta_list = gradientDescent(X,y)
-print(theta)
-
-plt.plot(error_list)
-plt.title("Reduction error over time")
-plt.show()
-y_ = hypothesis(X,theta)
-print(y_)
-
-
-# Training + Predictions
-plt.scatter(X,y)
-plt.plot(X,y_,color='orange',label="Prediction")
-plt.legend()
+# Visualizing the cost function
+plt.plot(np.arange(n), cost)
+plt.title("Cost function vs iterations")
+plt.xlabel("Iterations")
+plt.ylabel("Cost")
 plt.show()
 
-# Load the test data
-X_test = pd.read_csv('./Test/Linear_X_Test.csv').values
-y_test = hypothesis(X_test,theta)
-df = pd.DataFrame(data=y_test,columns=["y"])
-df.to_csv('y_prediction.csv',index=False)
+# Testing the model
+X_test = pd.read_csv('Linear Regression\python\Test\Linear_X_Test.csv').values # Loading data
+y_test = np.dot(X_test, theta[1:].transpose()) # Computing the predictions
+df = pd.DataFrame(data=y_test, columns=["y"]) # Converting to a dataframe
+df.to_csv('Linear Regression\python\y_prediction.csv', index=False) # Saving the dataframe
