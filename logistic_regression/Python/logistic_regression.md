ec379ec6a31c336a4227ef5c595c86b4add89dab
Nitin Sai <musicssns@gmail.com>
2020-10-03 11:05:14 +0530

    logistic regression

---
+++ logistic_regression/Python/logistic_regression.md
@@ -0,0 +1,9 @@
+# Logistic Regression
+
+This algorithm is used for classification. It is named after the function used in the method (Logistic function).
+
+<img src="">
+
+It’s an S-shaped curve that can take any real-valued number and map it into a value between 0 and 1, but never exactly at those limits.
+
+Logistic regression uses an equation as the representation, very much like [linear regression.]()
--- logistic_regression/Python/logistic_regression.py
+++ logistic_regression/Python/logistic_regression.py
@@ -1,88 +1,147 @@
+# Importing the dependancies
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_classification
-import seaborn as sns
 
+# Creating random data with 3 input features and 2 output features
+X = np.random.randn(3, 1000)
+y = np.random.randint(2, size=1000)
 
-# Sigmoid Function
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+# Defining the sigmoid function
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
 
+def forward_prop(X, y, params):
+    
+    """"
+    Propagate forward by computing:
+    A = sigmoid(W.X + b)
+    
+    Arguments:
+    X -- input data (num_of_features, training_size)
+    y -- input data (1, training_size)
+    W -- weights (num_of_features, 1)
+    b -- scalar value
+    
+    Returns:
+    A -- the prediction
+    cost -- the cost function
+    """
+    
+    m = X.shape[1]
+    
+    Z = np.dot(params['W'].T, X) + params['b']
+    A = sigmoid(Z)
+    
+    cost = -(1/m)*np.sum((y*np.log(A) + (1-y)*np.log(1-A)))
+    
+    return A, cost
 
-# Implementation of the stochastic gradient descent
-def gradient_descent(X, y, params, learning_rate, iterations):
-    m = len(y)
-    cost_history = np.zeros((iterations, 1))
+def back_prop(X, A):
+    
+    """
+    Propagate backwards by computing the gradients:
+    dA = -(y/A) + (1-y)/(1-A)
+    dZ = A - y
+    dW = (1/m)*X.dZ
+    db = (1/m)*sum(dZ)
+    
+    Arguments:
+    X -- input data (num_of_features, training_size)
+    A -- the prediction
+    
+    Returns:
+    dW, db -- gradients with same shape as W, b respectively
+    """
+    
+    m = X.shape[1]
+    
+    dZ = A - y
+    dW = (1/m)*np.dot(X, dZ.T)
+    db = (1/m)*np.sum(dZ)
+    
+    grads = {'dW': dW, 'db': db}
+    
+    return grads
 
-    for i in range(iterations):
-        params = params - (learning_rate/m) * (X.T @ (sigmoid(X @ params) - y)) 
-        cost_history[i] = compute_cost(X, y, params)
+def update_parameters(params, grads, learning_rate=0.001):
+    
+    """
+    Update the parameters:
+    W = W - lr*dW
+    b = b - lr*db
+    
+    Arguments:
+    W -- weights (num_of_features, 1)
+    b -- scalar value
+    dW, db -- gradients
+    learning_rate -- the rate at which the params should update (default = 0.001)
+    
+    Returns:
+    W, b -- updated parameters
+    """
+    
+    params['W'] -= learning_rate*grads['dW']
+    params['b'] -= learning_rate*grads['db']
+    
+    return params
 
-    return (cost_history, params)
+def model(X, y, epochs, learning_rate=0.001, visualize=False):
+    
+    """
+    Logistic Regression model
+    
+    Arguments:
+    X -- input data (num_of_features, training_size)
+    y -- input data (1, training_size)
+    learning_rate -- the rate at which the params should update (default = 0.001)
+    epochs -- number of iterations
+    visualize -- represent the loss on a graph (default = False)
+    
+    Returns:
+    W, b -- parameters
+    """
+    
+    # initialise the parameters
+    W = np.zeros((X.shape[0], 1))
+    b = 0
+    
+    params = {'W': W, 'b': b}
+    
+    # initialise the gradients
+    dW = np.zeros((X.shape[0], 1))
+    db = 0
+    
+    grads = {'dW': dW, 'db': db}
+    
+    cost = [0]*epochs
+    
+    # running the algorithm
+    for i in range(epochs):
+        
+        # forward prop
+        A, cost[i] = forward_prop(X, y, params)
+        
+        # printing the cost
+        if i%100 == 0:
+            print(f'loss after {i} iterations = {cost[i]}')
+        
+        # backwards prop
+        grads = back_prop(X, A)
+        
+        # updating the params
+        params = update_parameters(params, grads, learning_rate)
+        
+    if visualize:
+        
+        plt.style.use('fivethirtyeight')
+        
+        plt.plot(cost, label = 'cost')
+        plt.xlabel('Epochs')
+        plt.ylabel('Cost')
+        plt.legend()
+        plt.show()
+        
+    return params
 
-
-# Computing the cost via the negative log loss
-def compute_cost(X, y, theta):
-    m = len(y)
-    h = sigmoid(X @ theta)
-    epsilon = 1e-5
-    cost = (1/m)*(((-y).T @ np.log(h + epsilon))-((1-y).T @ np.log(1-h + epsilon)))
-    return cost
-
-
-def predict(X, params):
-    return np.round(sigmoid(X @ params))
-
-
-# Driver Code
-
-# Used scikit-learn to make a random binary classification dataset
-X, y = make_classification(n_samples=500, n_features=2, n_redundant=0, n_informative=1,
-                             n_clusters_per_class=1, random_state=14)
-
-y = y[:, np.newaxis]
-
-sns.set_style('white')
-sns.scatterplot(X[:, 0], X[:, 1], hue=y.reshape(-1))
-
-m = len(y)
-
-X = np.hstack((np.ones((m, 1)), X))
-n = np.size(X, 1)
-params = np.zeros((n, 1))
-
-iterations = 1000  # Number of iterations
-learning_rate = 0.01  # Learning rate of stochastic gradient descent
-
-initial_cost = compute_cost(X, y, params)
-
-print("Initial Cost is: {} \n".format(initial_cost))
-
-(cost_history, params_optimal) = gradient_descent(X, y, params, learning_rate, iterations)
-
-print("Optimal Parameters are: \n", params_optimal, "\n")
-
-plt.figure()
-sns.set_style('white')
-plt.plot(range(len(cost_history)), cost_history, 'r')
-plt.title("Convergence Graph of Cost Function")
-plt.xlabel("Number of Iterations")
-plt.ylabel("Cost")
-plt.show()
-
-y_pred = predict(X, params_optimal)
-score = float(sum(y_pred == y)) / float(len(y))
-
-print("Score is : ", score)
-
-slope = -(params_optimal[1] / params_optimal[2])
-intercept = -(params_optimal[0] / params_optimal[2])
-
-sns.set_style('white')
-sns.scatterplot(X[:, 1], X[:, 2], hue=y.reshape(-1))
-
-ax = plt.gca()
-ax.autoscale(False)
-x_vals = np.array(ax.get_xlim())
-y_vals = intercept + (slope * x_vals)
-plt.plot(x_vals, y_vals, c="k")
+# Calling the function
+p = model(X, y, 10000, visualize=True)
